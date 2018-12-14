58a9e3172ede751621b33ebd54812594bd495c47
EspeIgira <espeigira@gmail.com>
2018-12-14 14:00:44 +0200

    function if condition and else if condition

--- triangle.js
+++ triangle.js
@@ -4,15 +4,7 @@
    var side2= document.getElementById('sideb').value;
    var side3= document.getElementById('sidec').value;
 
-   /*Side1= (a + b);
-
-   Side2= (a + c);
-
-   side3= (c + b);*/
-    
-
-   if((side1===side2 && side2===side3 && side1===side3)){
-    alert("Equilateral");
+  
 
    }
    else if((side1===side2 || side2===side3 || side1===side3)){
