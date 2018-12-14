a06c0d979bfc5c6f41f036f8bf23a6f16d679c60
EspeIgira <espeigira@gmail.com>
2018-12-14 14:06:32 +0200

    declare all Variables as side1, side2, side 3

--- triangle.js
+++ triangle.js
@@ -2,9 +2,10 @@
  {
    var side1 = document.getElementById('sidea').value;
    var side2= document.getElementById('sideb').value;
-   var side3= document.getElementById('sidec').value;
+   var side3= document.ElementById('sidec').value;
 
-  
+   if((side1===side2 && side2===side3 && side1===side3)){
+    alert("Equilateral");
 
    }
    else if((side1===side2 || side2===side3 || side1===side3)){
