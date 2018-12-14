8e3f886cabbad9686b835ca8a6da3fc3462cdc8b
EspeIgira <espeigira@gmail.com>
2018-12-14 14:08:50 +0200

    Correct id as getElementById for the last variable

--- triangle.js
+++ triangle.js
@@ -2,7 +2,7 @@
  {
    var side1 = document.getElementById('sidea').value;
    var side2= document.getElementById('sideb').value;
-   var side3= document.ElementById('sidec').value;
+   var side3= document.getElementById('sidec').value;
 
    if((side1===side2 && side2===side3 && side1===side3)){
     alert("Equilateral");
