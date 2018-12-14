a6cbb6dd6e6710f9c1c735b7f7a249c2169f4a57
EspeIgira <espeigira@gmail.com>
2018-12-14 16:06:53 +0200

    Added README.md

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Triangle-tracker
+  
+  Tracking triangle sides.
+
+## Description
+{This is Tracking triangles if there are isoscele or equilateral or scalene}, {Created 14th December 2018}
+
+## Setup/Installation Requirements
+Browser and text editor.
+
+## Technologies Used
+HTML Language, JavaScript  and Css Stylesheet.
+  
+## Authors
+
+* **EspeIgira** - *Initial work* - [EspeIgira](https://github.com/EspeIgira/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+*{copyright &copy:2018 Triangle Tracker.}**
+
--- triangle.js
+++ triangle.js
@@ -1,21 +1,21 @@
- var triangle = 20;
+
  function triangle()
  {
-  var side = [side1,side2,side3]
+  /*var side = [side1,side2,side3]*/
 
 
-   var side1= document.getElementById('sidea').value;
-   var side2= document.getElementById('sideb').value;
-   var side3= document.getElementById('sidec').value;
+   var side1= document.getElementById('sideA').value;
+   var side2= document.getElementById('sideB').value;
+   var side3= document.getElementById('sideC').value;
 
-   if((side1===side2 && side2===side3 && side1===side3)){
+   if(side1===side2>0 && side2===side3>0 && side1===side3>0){
     alert("Equilateral");
 
    }
-   else if((side1===side2 || side2===side3 || side1===side3)){
+   else if(side1===side2 || side2===side3 || side1===side3){
      alert("Isoscele");
    }
-   else if((side1!==side2 || side2!==side3 || side1!==side3)){
+   else if(side1!==side2 || side2!==side3 || side1!==side3){
     alert("Scalene");
    }
    else{
