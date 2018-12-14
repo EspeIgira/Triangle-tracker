0faa2e15bbbded44da298ca56961bbd142271f2f
EspeIgira <espeigira@gmail.com>
2018-12-14 13:14:03 +0200

    Triangle Tracker

--- triangle.js
+++ triangle.js
@@ -1,5 +1,27 @@
-function myFunction()
-{
-var firstString = document.getElementById('myNumber').value;
-alert(firstString);
-}
+ function triangle()
+ {
+   var side1 = document.getElementById('sidea').value;
+   var side2= document.getElementById('sideb').value;
+   var side3= document.getElementById('sidec').value;
+
+   /*Side1= (a + b);
+
+   Side2= (a + c);
+
+   side3= (c + b);*/
+    
+
+   if((side1===side2 && side2===side3 && side1===side3)){
+    alert("Equilateral");
+
+   }
+   else if((side1===side2 || side2===side3 || side1===side3)){
+     alert("Isoscele");
+   }
+   else if((side1!==side2 || side2!==side3 || side1!==side3)){
+    alert("Scalene");
+   }
+   else{
+     alert("Not a triangle");
+   }
+ }
